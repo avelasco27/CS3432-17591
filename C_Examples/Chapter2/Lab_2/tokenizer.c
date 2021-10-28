2e7639e4788f7a46e0b74ad4785909722353fb05
avelasco27 <90271260+avelasco27@users.noreply.github.com>
2021-10-28 12:30:48 -0600

    Update tokenizer.c

--- C_Examples/Chapter2/Lab_2/tokenizer.c
+++ C_Examples/Chapter2/Lab_2/tokenizer.c
@@ -7,20 +7,6 @@
 /* Return true (non-zero) if c is a whitespace character
 ('\t' or ' '), Zero terminators are not printable (therefore false)*/
 
-// bool delim_character(char c){
-// 	if(c ==' ' || c == '\t'){
-// 		return true;
-// 	}else{
-// 		return false;
-// 	}
-// }
-// bool non_delim_character(char c){
-// 	if(c ==' ' || c == '\t'){
-// 		return false;
-// 	}else{
-// 		return true;
-// 	}
-// }
 /* Return true (non-zero) if c is a whitespace characer
 
    ('\t' or ' ').
@@ -52,21 +38,6 @@ bool non_delim_character(char c){
 }
 
 
-
-/* Returns a pointer to the first character of the next
-
-   space-separated word*/
-
-// char *word_start(char* str){
-//   for(int i = 0; *(str+i) != '\0'; i++){
-//       if(non_delim_character(*(str+i))){
-// 	return str+i;
-//       }
-//     }
-// }
-
-
-
 /* Returns a pointer to the first space character of the zero
 
    terminated string*/
@@ -86,41 +57,6 @@ char *end_word(char* str){
 		}
 	}
 }
-// int count_tokens(char* str){
-// 	int counter =0;
-// 	for(int i=0; *(str+i)!='\0'; i++){
-// 		if(non_delim_character(*(str+1))){
-// 			if(delim_character(*(str+i+1))){
-// 				counter++;
-// 			}
-// 		}
-// 	}
-// 	return counter;
-// }
-// char *copy_str(char *inStr, short len){
-// 	char *copy = (char *)malloc(len);
-// 	for(int i =0; i<len; i++){
-// 		*(copy+i) = *(inStr+i);
-// 	}
-// 		*(copy+len) = '\n';
-// 		return copy;
-// }
-// char** tokenize(char* str, char delim []){
-// 	char words = count_tokens(str)+1;
-// 	char **arr = (char **)malloc(sizeof(char *) * (words+1));
-// 	int i = 0;
-// 	arr[i] = strtok(str, delim);
-// 	for(i = 1; i < words; i++){
-// 		arr[i] = strtok(NULL, delim);
-// 	}
-// 	arr[i++] ='\0';
-// 	return arr;
-// }
-// void print_all_tokens(char** tokens){
-// 	for(int i =0; *(tokens+i) != '\0'; i++){
-// 		printf("%s\n", *(tokens+i));
-// 	}
-// }
  
 
   char *start = word_start(str);
